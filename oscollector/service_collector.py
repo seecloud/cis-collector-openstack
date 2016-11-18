5ebb62d4533810c1c53a89b73367c6d24c46c5dc
Volodymyr Stoiko <vstoiko@mirantis.com>
2016-11-18 22:01:47 +0200

    Collectors for nova, neutron, glance, cinder, heat

--- oscollector/service_collector.py
+++ oscollector/service_collector.py
@@ -60,6 +60,181 @@ class ServiceCollector:
         return keystone_state
 
     @classmethod
+    def __collect_nova(cls, client, services):
+        """Nova collector. Not used directly. Called from collect function.
+
+        :param client: OpenStack client with proper session
+        :param services: list of services for collecting data from
+        :return nova_state: dict with collected data by services as keys
+        """
+
+        nc = client.nova
+        nova_state = collections.defaultdict(dict)
+
+        for service in services:
+            if service in ['flavors', 'hypervisors', 'images', 'keypairs']:
+                for item in nc.__dict__[service].list():
+                    nova_state[service].update(
+                        resource_factory.ResourceFactory
+                        .create_resource(item, 'nova', service))
+
+            elif service == 'servers':
+                for item in nc.servers.list(search_opts={'all_tenants': 1}):
+                    nova_state[service].update(
+                        resource_factory.ResourceFactory
+                        .create_resource(item, 'nova', service))
+
+            elif service == 'quotas':
+                item = nc.quotas.get(None)
+                nova_state[service].update(
+                    resource_factory.ResourceFactory
+                    .create_resource(item, 'nova', service))
+
+            else:
+                logger.warning(" ".join(["There is no {0} service in nova"
+                                        .format(service),
+                                         "It will be removed",
+                                         "from collector configuration"]),
+                               exc_info=True)
+
+        return nova_state
+
+    @classmethod
+    def __collect_neutron(cls, client, services):
+        """Neutron collector. Not used directly. Called from collect function.
+
+        :param client: OpenStack client with proper session
+        :param services: list of services for collecting data from
+        :return neutron_state: dict with collected data by services as keys
+        """
+
+        nc = client.neutron
+        neutron_state = collections.defaultdict(dict)
+
+        neutron_services = {
+            'networks': nc.list_networks,
+            'routers': nc.list_routers,
+            'subnets': nc.list_subnets,
+            'ports': nc.list_ports,
+            'security_groups': nc.list_security_groups,
+            'security_group_rules': nc.list_security_group_rules,
+            'floatingips': nc.list_floatingips,
+            'quotas': nc.list_quotas,  # Todo: add schema
+            'flavors': nc.list_flavors  # Todo: add schema
+        }
+
+        for service in services:
+
+            service_data = neutron_services.get(service,
+                                                lambda *args: None)()
+
+            if not service_data:
+                logger.warning(" ".join(["There is no {0} service in neutron."
+                                        .format(service),
+                                         "It will be removed",
+                                         "from collector configuration"]),
+                               exc_info=True)
+                neutron_state[service] = settings.SERVICE_INVALID_STATE
+            else:
+
+                for neutron_resource, values in service_data.items():
+                    for item in values:
+                        neutron_state[neutron_resource].update(
+                            resource_factory.ResourceFactory
+                            .create_resource(item, 'neutron',
+                                             neutron_resource))
+
+        return neutron_state
+
+    @classmethod
+    def __collect_glance(cls, client, services):
+        """Glance collector. Not used directly. Called from collect function.
+
+        :param client: OpenStack client with proper session
+        :param services: list of services for collecting data from
+        :return glance_state: dict with collected data by services as keys
+        """
+
+        gc = client.glance
+        glance_state = collections.defaultdict(dict)
+
+        for service in services:
+            try:
+                service_client = gc.__dict__[service]
+                for item in service_client.list():
+                    glance_state[service].update(
+                        resource_factory.ResourceFactory
+                        .create_resource(item, 'glance', service))
+            except KeyError:
+                logger.warning(" ".join(["There is no {0} service in glance."
+                                        .format(service),
+                                         "It will be removed",
+                                         "from collector configuration"]),
+                               exc_info=True)
+                glance_state[service] = settings.SERVICE_INVALID_STATE
+
+        return glance_state
+
+    @classmethod
+    def __collect_cinder(cls, client, services):
+        """Cinder collector. Not used directly. Called from collect function.
+
+        :param client: OpenStack client with proper session
+        :param services: list of services for collecting data from
+        :return cinder_state: dict with collected data by services as keys
+        """
+
+        cc = client.cinder
+        cinder_state = collections.defaultdict(dict)
+
+        # Todo: find cinder backups structure ?
+
+        for service in services:
+            try:
+                service_client = cc.__dict__[service]
+                for item in (service_client
+                             .list(search_opts={'all_tenants': 1})):
+                    cinder_state[service].update(
+                        resource_factory.ResourceFactory
+                        .create_resource(item, 'cinder', service))
+            except KeyError:
+                logger.warning(" ".join(["There is no {0} service in cinder."
+                                        .format(service),
+                                         "It will be removed",
+                                         "from collector configuration"]),
+                               exc_info=True)
+                cinder_state[service] = settings.SERVICE_INVALID_STATE
+        return cinder_state
+
+    @classmethod
+    def __collect_heat(cls, client, services):
+        """Heat collector. Not used directly. Called from collect function.
+
+        :param client: OpenStack client with proper session
+        :param services: list of services for collecting data from
+        :return heat_state: dict with collected data by services as keys
+        """
+
+        hc = client.heat
+        heat_state = collections.defaultdict(dict)
+
+        for service in services:
+            try:
+                service_client = hc.__dict__[service]
+                for item in service_client.list():
+                    data = item.__dict__.get('_info')
+                    heat_state[service][str(data.pop('id'))] = data
+            except KeyError:
+                logger.warning(" ".join(["There is no {0} service in heat."
+                                        .format(service),
+                                         "It will be removed",
+                                         "from collector configuration"]),
+                               exc_info=True)
+                heat_state[service] = settings.SERVICE_INVALID_STATE
+
+        return heat_state
+
+    @classmethod
     def collect(cls, client, component, services):
         """Wrapper around other OpenStack component collectors
 
@@ -84,6 +259,11 @@ class ServiceCollector:
     @classmethod
     def get_collector(cls, component):
         colectors = {
-            'keystone': cls.__collect_keystone
+            'keystone': cls.__collect_keystone,
+            'nova': cls.__collect_nova,
+            'neutron': cls.__collect_neutron,
+            'glance': cls.__collect_glance,
+            'cinder': cls.__collect_cinder,
+            'heat': cls.__collect_heat
         }
         return colectors.get(component, lambda *args: None)
